9922f6b48d70c498a9481622c2a1f3eaedf0f1bb
amalipalal <palal.asare@amalitech.com>
2025-12-18 10:47:54 +0000

    feat: implement application models
    - Category
    - Customer
    - OrderItem
    - Orders
    - Product
    - Review
    - Role
    - Users

---
+++ src/main/java/org/example/model/Product.java
@@ -0,0 +1,94 @@
+package org.example.model;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class Product {
+    private UUID productId;
+    private String name;
+    private String description;
+    private Double price;
+    private int stockQuantity;
+    private UUID categoryId;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public Product() {}
+
+    public Product(UUID productId, String name, String description, Double price,
+                   int stockQuantity, UUID categoryId, Instant createdAt, Instant updatedAt
+    ) {
+        this.productId = productId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.categoryId = categoryId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public UUID getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(UUID categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
