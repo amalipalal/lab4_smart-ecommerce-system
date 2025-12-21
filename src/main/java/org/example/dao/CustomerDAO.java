d88ce9b65edd833cd3ec04081ae4c3c99b3a346b
amalipalal <palal.asare@amalitech.com>
2025-12-21 19:37:30 +0000

    feat: add exception throwing to interface methods

--- src/main/java/org/example/dao/CustomerDAO.java
+++ src/main/java/org/example/dao/CustomerDAO.java
@@ -1,5 +1,6 @@
 package org.example.dao;
 
+import org.example.dao.exception.DAOException;
 import org.example.model.Customer;
 
 import java.util.Optional;
@@ -7,11 +8,11 @@ import java.util.UUID;
 
 public interface CustomerDAO {
 
-    Optional<Customer> findById(UUID customerId);
+    Optional<Customer> findById(UUID customerId) throws DAOException;
 
-    Optional<Customer> findByEmail(String email);
+    Optional<Customer> findByEmail(String email) throws DAOException;
 
-    void save(Customer customer);
+    void save(Customer customer) throws DAOException;
 
-    void update(Customer customer);
+    void update(Customer customer) throws DAOException;
 }
