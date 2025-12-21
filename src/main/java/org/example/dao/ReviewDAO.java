d7f1001acfe83fbbf80db4ee1cbda3718e9c4278
amalipalal <palal.asare@amalitech.com>
2025-12-21 21:16:27 +0000

    feat: add DAOException throws to ReviewDAO

--- src/main/java/org/example/dao/ReviewDAO.java
+++ src/main/java/org/example/dao/ReviewDAO.java
@@ -1,5 +1,6 @@
 package org.example.dao;
 
+import org.example.dao.exception.DAOException;
 import org.example.model.Review;
 
 import java.util.List;
@@ -7,9 +8,9 @@ import java.util.UUID;
 
 public interface ReviewDAO {
 
-    List<Review> findByProduct(UUID productId, int limit, int offset);
+    List<Review> findByProduct(UUID productId, int limit, int offset) throws DAOException;
 
-    void save(Review review);
+    void save(Review review) throws DAOException;
 
-    void delete(UUID reviewId);
+    void delete(UUID reviewId) throws DAOException;
 }
