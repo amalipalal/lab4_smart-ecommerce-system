dd2ae24c11eb10a02e929ba0d210c2ddc8a210d5
amalipalal <palal.asare@amalitech.com>
2025-12-22 14:46:27 +0000

    feat: add createCategory method to CategoryService

---
+++ src/main/java/org/example/service/CategoryService.java
@@ -0,0 +1,42 @@
+package org.example.service;
+
+import org.example.dao.CategoryDAO;
+import org.example.dao.exception.DAOException;
+import org.example.dto.category.CreateCategoryRequest;
+import org.example.dto.category.CreateCategoryResponse;
+import org.example.model.Category;
+import org.example.service.exception.CategoryNotFoundException;
+import org.example.service.exception.DuplicateCategoryException;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class CategoryService {
+
+    private final CategoryDAO categoryDAO;
+
+    public CategoryService(CategoryDAO categoryDAO) {
+        this.categoryDAO = categoryDAO;
+    }
+
+    public CreateCategoryResponse createCategory(CreateCategoryRequest request) {
+        try {
+            if (this.categoryDAO.findByName(request.name()).isPresent())
+                throw new DuplicateCategoryException(request.name());
+
+            var category = new Category(
+                    UUID.randomUUID(),
+                    request.name(),
+                    request.description(),
+                    Instant.now(),
+                    Instant.now()
+            );
+            this.categoryDAO.save(category);
+
+            return new CreateCategoryResponse(
+                    category.getCategoryId(), category.getName(), category.getDescription());
+        } catch (DAOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+}
