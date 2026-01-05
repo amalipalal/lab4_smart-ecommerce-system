13f3d1a08e997887f6e5142d622abb49a6210a2f
amalipalal <palal.asare@amalitech.com>
2026-01-05 11:24:08 +0000

    chore: setup javafx packages and dependencies (#7)
    * chore: add fxml dependencies to pom.xml
    
    - javafx-controls
    - javafx-fxml
    - javafx-maven-plugin
    
    * chore: add test css file
    
    * chore: add test fxml file
    
    * chore: update Main to launch GUI with fxml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/test-view.fxml")
+        );
+
+        Parent root = loader.load();
+
+        stage.setTitle("E-commerce App");
+        stage.setScene(new Scene(root, 1000, 600));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
