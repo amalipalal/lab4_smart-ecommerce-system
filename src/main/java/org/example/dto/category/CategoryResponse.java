36153786f2ae3432da3a134a39a1843a973174bd
amalipalal <palal.asare@amalitech.com>
2026-01-06 14:45:36 +0000

    feat: create dtos for request and response of category update

---
+++ src/main/java/org/example/dto/category/CategoryResponse.java
@@ -0,0 +1,23 @@
+package org.example.dto.category;
+
+import org.example.model.Category;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record CategoryResponse(
+        UUID categoryId,
+        String name,
+        String description,
+        Instant createdAt,
+        Instant updatedAt
+) {
+    public CategoryResponse(Category category) {
+        this(
+                category.getCategoryId(),
+                category.getName(),
+                category.getDescription(),
+                category.getCreatedAt(),
+                category.getUpdatedAt());
+    }
+}
