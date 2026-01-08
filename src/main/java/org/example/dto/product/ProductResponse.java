828b86b10dbd775f4a06aeac2d556680ca8dc915
amalipalal <palal.asare@amalitech.com>
2026-01-08 16:32:37 +0000

    feat: add categoryId and updatedAt to ProductResponse

--- src/main/java/org/example/dto/product/ProductResponse.java
+++ src/main/java/org/example/dto/product/ProductResponse.java
@@ -2,18 +2,21 @@ package org.example.dto.product;
 
 import org.example.model.Product;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public record ProductResponse(
         UUID productId,
+        UUID categoryId,
         String name,
         String description,
         double price,
-        int stock
+        int stock,
+        Instant updatedAt
 ) {
 
     public ProductResponse(Product product) {
-        this(product.getProductId(), product.getName(), product.getDescription(),
-                product.getPrice(), product.getStockQuantity());
+        this(product.getProductId(), product.getCategoryId(), product.getName(), product.getDescription(),
+                product.getPrice(), product.getStockQuantity(), product.getUpdatedAt());
     }
 }
