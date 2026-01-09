1c823c27dab0066dfa373911c70f8cd2296d1115
amalipalal <palal.asare@amalitech.com>
2026-01-09 22:01:44 +0000

    feat: centralised shell routing (#19)
    * feat: add centralised admin and buyer router in Router
    
    * feat: add shell page controller for sidepane routing
    
    * refactor: update position of fxml files
    
    - contains ui update for admin-category.fxml and admin-product.fxml
    
    * feat: initialize router an first fxml file in Main.java

---
+++ src/main/java/org/example/controller/AdminShellController.java
@@ -0,0 +1,37 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+import org.example.ui.Router;
+
+public class AdminShellController {
+
+    @FXML
+    private StackPane contentArea;
+
+    @FXML
+    public void initialize() {
+        contentArea.getChildren().setAll(
+                Router.goToAdmin("admin-product.fxml")
+        );
+    }
+
+    @FXML
+    private void goProducts() {
+        contentArea.getChildren().setAll(
+                Router.goToAdmin("admin-product.fxml")
+        );
+    }
+
+    @FXML
+    private void goCategories() {
+        contentArea.getChildren().setAll(
+                Router.goToAdmin("admin-category.fxml")
+        );
+    }
+
+    @FXML
+    private void goBuyer() {
+        Router.goToBuyer("buyer-home.fxml");
+    }
+}
