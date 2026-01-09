b1f825ca18328ff55811730c5535b281a3fdc3d7
amalipalal <palal.asare@amalitech.com>
2026-01-09 21:57:39 +0000

    feat: initialize router an first fxml file in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.ui.Router;
 
 public class Main extends Application {
     @Override
@@ -12,14 +13,17 @@ public class Main extends Application {
         ApplicationContext  context = ApplicationContext.getInstance();
         ApplicationControllerFactory factory = new ApplicationControllerFactory(context);
 
+        Router.init(factory);
+
         FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/fxml/admin-product.fxml")
+                getClass().getResource("/fxml/admin/admin-shell.fxml")
         );
         loader.setControllerFactory(factory);
         Parent root = loader.load();
 
         stage.setTitle("E-commerce App");
         stage.setScene(new Scene(root, 1000, 600));
+
         stage.show();
     }
 
