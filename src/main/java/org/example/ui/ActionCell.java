3c30ff874e74f7b435acc81bd65cfcb74fa813de
amalipalal <palal.asare@amalitech.com>
2026-01-09 18:33:27 +0000

    feat: add action button definition classes
    - ActionCell
    - ActionDefinition

---
+++ src/main/java/org/example/ui/ActionCell.java
@@ -0,0 +1,31 @@
+package org.example.ui;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.layout.HBox;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+import java.util.List;
+
+public class ActionCell<T> extends TableCell<T, Void> {
+    private final HBox container = new HBox(5);
+
+    public ActionCell(List<ActionDefinition<T>> actions) {
+        for (ActionDefinition<T> def : actions) {
+            Button btn = new Button();
+            FontIcon icon = new FontIcon(def.iconName());
+            icon.setIconSize(def.iconSize());
+            btn.setGraphic(icon);
+            btn.getStyleClass().add(def.styleClass());
+            btn.setOnAction(e -> def.action().accept(getTableView().getItems().get(getIndex())));
+            container.getChildren().add(btn);
+        }
+    }
+
+    @Override
+    protected void updateItem(Void item, boolean empty) {
+        super.updateItem(item, empty);
+        setGraphic(empty ? null : container);
+    }
+}
+
