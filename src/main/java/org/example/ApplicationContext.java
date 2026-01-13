3008dfc6e70088963e87d7f1d8e3b811f7f84640
amalipalal <palal.asare@amalitech.com>
2026-01-13 16:58:39 +0000

    feat: update app instantiaction of CategoryService

--- src/main/java/org/example/ApplicationContext.java
+++ src/main/java/org/example/ApplicationContext.java
@@ -1,10 +1,10 @@
 package org.example;
 
 import org.example.cache.ProductCache;
+import org.example.dao.impl.SqlCategoryWriteDaoFactory;
 import org.example.dao.impl.category.SqlCategoryReadDao;
 import org.example.dao.impl.product.SqlProductReadDao;
-import org.example.dao.interfaces.CategoryDAO;
-import org.example.dao.impl.CategoryJdbcDAO;
+import org.example.dao.interfaces.CategoryWriteDaoFactory;
 import org.example.dao.interfaces.category.CategoryReadDao;
 import org.example.dao.interfaces.product.ProductReadDao;
 import org.example.service.CategoryService;
@@ -13,18 +13,18 @@ import org.example.service.ProductService;
 public class ApplicationContext {
 
     private static ApplicationContext instance;
-
     private final CategoryService categoryService;
     private final ProductService productService;
 
     private ApplicationContext() {
-        CategoryDAO categoryDOA = new CategoryJdbcDAO();
         CategoryReadDao categoryReadDao = new SqlCategoryReadDao();
         ProductReadDao productReadDao = new SqlProductReadDao();
 
         var cache = new ProductCache();
 
-        this.categoryService = new CategoryService(categoryReadDao, new SqlUnitOfWorkFactory(), cache);
+        CategoryWriteDaoFactory categoryWriteFactory = new SqlCategoryWriteDaoFactory();
+
+        this.categoryService = new CategoryService(categoryReadDao, new SqlUnitOfWorkFactory(), categoryWriteFactory, cache);
         this.productService = new ProductService(productReadDao, new ProductCache(), new SqlUnitOfWorkFactory());
     }
 
