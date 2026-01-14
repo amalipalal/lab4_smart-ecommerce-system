4e08b48900ef652baf3d98b7979af60a1841f534
amalipalal <palal.asare@amalitech.com>
2026-01-14 21:36:51 +0000

    feat: add OrderService place order method

--- src/main/java/org/example/service/OrderService.java
+++ src/main/java/org/example/service/OrderService.java
@@ -1,12 +1,22 @@
 package org.example.service;
 
+import org.example.cache.ProductCache;
 import org.example.dao.exception.DAOException;
 import org.example.dao.interfaces.OrdersDAO;
+import org.example.dao.interfaces.ProductWriteDaoFactory;
+import org.example.dao.interfaces.order.OrderWriteDao;
+import org.example.dao.interfaces.order.OrderWriteDaoFactory;
 import org.example.dao.interfaces.product.ProductDAO;
+import org.example.dao.interfaces.product.ProductReadDao;
+import org.example.dao.interfaces.product.ProductWriteDao;
 import org.example.dto.order.CustomerDetails;
 import org.example.dto.order.OrderRequest;
 import org.example.model.Orders;
 import org.example.model.Product;
+import org.example.persistence.UnitOfWork;
+import org.example.persistence.UnitOfWorkFactory;
+import org.example.service.exception.InsufficientProductStock;
+import org.example.service.exception.ProductNotFoundException;
 import org.example.util.data.DBConnection;
 import org.example.util.exception.DatabaseConnectionException;
 
@@ -16,8 +26,28 @@ import java.time.Instant;
 import java.util.UUID;
 
 public class OrderService {
-    private final OrdersDAO ordersDAO;
-    private final ProductDAO productDAO;
+    private OrdersDAO ordersDAO;
+    private ProductDAO productDAO;
+
+    private ProductReadDao productReadDao;
+    private UnitOfWorkFactory unitOfWorkFactory;
+    private ProductWriteDaoFactory productWriteDaoFactory;
+    private OrderWriteDaoFactory orderWriteDaoFactory;
+    private ProductCache cache;
+
+    public OrderService(
+            ProductReadDao productReadDao,
+            UnitOfWorkFactory unitOfWorkFactory,
+            ProductWriteDaoFactory productWriteDaoFactory,
+            OrderWriteDaoFactory orderWriteDaoFactory,
+            ProductCache cache
+    ) {
+        this.productReadDao = productReadDao;
+        this.unitOfWorkFactory = unitOfWorkFactory;
+        this.productWriteDaoFactory = productWriteDaoFactory;
+        this.orderWriteDaoFactory = orderWriteDaoFactory;
+        this.cache = cache;
+    }
 
     public OrderService(OrdersDAO ordersDAO, ProductDAO productDAO) {
         this.ordersDAO = ordersDAO;
@@ -25,6 +55,55 @@ public class OrderService {
     }
 
     public void placeOrder(OrderRequest orderRequest, CustomerDetails customerDetails) {
+        // create a unit of work object to represent a single transaction
+        UnitOfWork unitOfWork = unitOfWorkFactory.create();
+        try {
+            Product product = retrieveProduct(orderRequest);
+            if(product.getStockQuantity() - orderRequest.quantity() < 0)
+                throw new InsufficientProductStock(product.getProductId().toString());
+
+            ProductWriteDao productWriteDao = this.productWriteDaoFactory.create(unitOfWork.getConnection());
+            productWriteDao.reduceStock(product.getProductId(), orderRequest.quantity());
+
+            storeProduct(orderRequest, unitOfWork, product);
+
+            unitOfWork.commit();
+        } catch (DAOException e) {
+            unitOfWork.rollback();
+            throw e;
+        } finally {
+            unitOfWork.close();
+        }
+    }
+
+    private void storeProduct(OrderRequest orderRequest, UnitOfWork unitOfWork, Product product) {
+        double totalPrice = product.getPrice() * orderRequest.quantity();
+        Orders order = createOrder(orderRequest, null, totalPrice); // TODO: Add customer id
+
+        OrderWriteDao orderWriteDao = this.orderWriteDaoFactory.create(unitOfWork.getConnection());
+        orderWriteDao.save(order);
+    }
+
+    private Orders createOrder(OrderRequest orderRequest, UUID customerId, double totalPrice) {
+        return new Orders(
+                UUID.randomUUID(),
+                customerId,
+                Instant.now(),
+                totalPrice,
+                orderRequest.shippingCountry(),
+                orderRequest.shippingCity(),
+                orderRequest.postalCode()
+        );
+    }
+
+    private Product retrieveProduct(OrderRequest orderRequest) {
+        UUID productId = orderRequest.productId();
+        String key = "product:" + productId.toString();
+        return this.cache.getOrLoad(key, () -> this.productReadDao.findById(productId)
+                .orElseThrow(() -> new ProductNotFoundException(productId.toString())));
+    }
+
+    public void oldPlaceOrder(OrderRequest orderRequest, CustomerDetails customerDetails) {
         try(Connection conn = DBConnection.getConnection()) {
             conn.setAutoCommit(false);
 
@@ -44,17 +123,5 @@ public class OrderService {
         } catch (SQLException | DatabaseConnectionException | DAOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private Orders createOrder(OrderRequest orderRequest, UUID customerId, double totalPrice) {
-        return new Orders(
-                UUID.randomUUID(),
-                customerId,
-                Instant.now(),
-                totalPrice,
-                orderRequest.shippingCountry(),
-                orderRequest.shippingCity(),
-                orderRequest.postalCode()
-        );
     }
 }
