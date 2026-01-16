6bd1f3cb80d17d62a62f64849577fe2a66de886c
amalipalal <palal.asare@amalitech.com>
2026-01-16 15:59:39 +0000

    refactor: category service (#32)
    * feat: add a CategoryStore to interface with Daos.
    
    * refactor: abstract Dao interaction from CategoryService
    
    * refactor: use new CategoryService

--- src/main/java/org/example/application/ApplicationContext.java
+++ src/main/java/org/example/application/ApplicationContext.java
@@ -2,21 +2,19 @@ package org.example.application;
 
 import org.example.config.DataSource;
 import org.example.config.DatabaseConfig;
+import org.example.dao.impl.category.CategoryJdbcDao;
 import org.example.dao.impl.customer.CustomerJdbcDao;
 import org.example.dao.impl.order.OrderJdbcDao;
 import org.example.dao.impl.product.ProductJdbcDao;
+import org.example.dao.interfaces.category.CategoryDao;
 import org.example.dao.interfaces.customer.CustomerDao;
 import org.example.dao.interfaces.order.OrdersDao;
 import org.example.dao.interfaces.product.ProductDao;
-import org.example.persistence.impl.sql.SqlUnitOfWorkFactory;
 import org.example.cache.ProductCache;
-import org.example.dao.impl.SqlCategoryWriteDaoFactory;
-import org.example.dao.impl.category.SqlCategoryReadDao;
-import org.example.dao.interfaces.CategoryWriteDaoFactory;
-import org.example.dao.interfaces.category.CategoryReadDao;
 import org.example.service.CategoryService;
 import org.example.service.PurchaseService;
 import org.example.service.ProductService;
+import org.example.store.CategoryStore;
 import org.example.store.OrderStore;
 import org.example.store.ProductStore;
 
@@ -28,37 +26,22 @@ public class ApplicationContext {
     private final PurchaseService purchaseService;
 
     private ApplicationContext() {
-        DataSource dataSource = new DataSource(
-                DatabaseConfig.DB_URL,
-                DatabaseConfig.DB_USER,
-                DatabaseConfig.DB_PASSWORD
-        );
+        DataSource dataSource = new DataSource(DatabaseConfig.DB_URL,
+                DatabaseConfig.DB_USER, DatabaseConfig.DB_PASSWORD);
 
         var cache = new ProductCache();
 
         CustomerDao customerDao = new CustomerJdbcDao();
         ProductDao productDao = new ProductJdbcDao();
         OrdersDao ordersDao = new OrderJdbcDao();
+        CategoryDao categoryDao = new CategoryJdbcDao();
 
-        OrderStore orderStore = new OrderStore(
-                dataSource,
-                cache,
-                customerDao,
-                productDao,
-                ordersDao
-        );
-        ProductStore productStore = new ProductStore(
-                dataSource,
-                cache,
-                productDao
-        );
+        OrderStore orderStore = new OrderStore(dataSource,
+                cache, customerDao, productDao, ordersDao);
+        ProductStore productStore = new ProductStore(dataSource, cache, productDao);
+        CategoryStore categoryStore = new CategoryStore(dataSource, cache, categoryDao);
 
-        CategoryReadDao categoryReadDao = new SqlCategoryReadDao();
-
-        CategoryWriteDaoFactory categoryWriteFactory = new SqlCategoryWriteDaoFactory();
-
-        this.categoryService = new CategoryService(categoryReadDao,
-                new SqlUnitOfWorkFactory(), categoryWriteFactory, cache);
+        this.categoryService = new CategoryService(categoryStore);
         this.productService = new ProductService(productStore);
         this.purchaseService = new PurchaseService(orderStore);
     }
