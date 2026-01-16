abd6097b1ada5099e4b5e83a563933de754db74a
amalipalal <palal.asare@amalitech.com>
2026-01-16 16:30:44 +0000

    chore: delete files not in use

--- src/main/java/org/example/dao/interfaces/CategoryWriteDaoFactory.java
+++
@@ -1,9 +0,0 @@
-package org.example.dao.interfaces;
-
-import org.example.dao.interfaces.category.CategoryWriteDao;
-
-import java.sql.Connection;
-
-public interface CategoryWriteDaoFactory {
-    CategoryWriteDao create (Connection connection);
-}
--- src/main/java/org/example/dao/interfaces/order/OrderWriteDaoFactory.java
+++
@@ -1,7 +0,0 @@
-package org.example.dao.interfaces.order;
-
-import java.sql.Connection;
-
-public interface OrderWriteDaoFactory {
-    OrderWriteDao create(Connection connection);
-}
