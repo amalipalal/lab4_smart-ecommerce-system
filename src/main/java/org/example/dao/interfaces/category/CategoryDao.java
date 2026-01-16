4232daa6641d63939c05965d06a9cce9cfad540a
amalipalal <palal.asare@amalitech.com>
2026-01-16 11:21:59 +0000

    refactor: introduce connection parameter to Dao contracts

---
+++ src/main/java/org/example/dao/interfaces/category/CategoryDao.java
@@ -0,0 +1,29 @@
+package org.example.dao.interfaces.category;
+
+import org.example.dao.exception.DAOException;
+import org.example.model.Category;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CategoryDao {
+
+    Optional<Category> findById(Connection connection, UUID categoryId) throws DAOException;
+
+    Optional<Category> findByName(Connection connection, String name) throws DAOException;
+
+    List<Category> searchByName(Connection connection, String query, int limit, int offset) throws DAOException;
+
+    int countByName(Connection connection, String query) throws DAOException;
+
+    List<Category> findAll(Connection connection, int limit, int offset) throws DAOException;
+
+    void save(Connection connection, Category category) throws DAOException;
+
+    void update(Connection connection, Category category) throws DAOException;
+
+    int count(Connection connection) throws DAOException;
+
+}
---
+++ src/main/java/org/example/dao/interfaces/order/OrdersDao.java
@@ -0,0 +1,18 @@
+package org.example.dao.interfaces.order;
+
+import org.example.dao.exception.DAOException;
+import org.example.model.Orders;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OrdersDao {
+
+    Optional<Orders> findById(Connection connection, UUID orderId) throws DAOException;
+
+    List<Orders> findByCustomer(Connection connection, UUID customerId, int limit, int offset) throws DAOException;
+
+    void save(Connection connection, Orders order) throws DAOException;
+}
