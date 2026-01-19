94738a9f242d59bdd7cda60700d489287f4c72ea
amalipalal <palal.asare@amalitech.com>
2026-01-19 16:29:38 +0000

    feat(store): add custom OrderStore exceptions

---
+++ src/main/java/org/example/store/order/exception/OrderCountException.java
@@ -0,0 +1,7 @@
+package org.example.store.order.exception;
+
+public class OrderCountException extends RuntimeException {
+    public OrderCountException(String message) {
+        super(message);
+    }
+}
